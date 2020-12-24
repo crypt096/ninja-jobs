6b1e7b000afd9f5d7fffc6d95455e29901ae1dff
crypt0 <acvjetan@gmail.com>
2020-12-24 22:58:28 +0100

    Add folder structure.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import {
 } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
 
 const routes = [{
     path: '/',
@@ -14,6 +15,11 @@ const routes = [{
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
   }
 ]
 
