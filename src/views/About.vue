da10863332fb61a2b80aedc308ab0126f46696e8
crypt0 <acvjetan@gmail.com>
2020-12-24 22:51:31 +0100

    Router links and remove unnecessary code.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima odio odit
+      ullam iste non ducimus quibusdam. Debitis incidunt vero itaque, explicabo
+      dignissimos doloribus similique culpa architecto? Assumenda aliquam
+      dolorem quisquam id blanditiis debitis voluptate accusantium. Placeat
+      facere eius eaque harum! Illo error eius sunt iste odio voluptatum
+      sapiente modi voluptas expedita at cumque illum molestiae dicta nesciunt
+      suscipit libero veniam inventore, ipsa explicabo, perspiciatis mollitia
+      fuga asperiores. Laborum praesentium sapiente dignissimos quo nemo quaerat
+      sit cum esse eaque obcaecati odio soluta voluptatibus veritatis
+      cupiditate, repudiandae consequuntur explicabo! Illum nesciunt dolore cum
+      atque, beatae quae similique quaerat quam non cumque in.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam nam
+      adipisci distinctio voluptate repellendus harum recusandae, accusamus
+      similique at voluptatum minima, aliquam pariatur minus neque, quo dolore
+      voluptatibus maxime omnis! Rem culpa quas odio voluptatibus quis. Corrupti
+      deserunt quibusdam impedit! Corporis ex fugiat in corrupti totam, tenetur
+      nulla reiciendis. Beatae ipsam possimus vel optio, iste distinctio amet
+      magni facere. Harum impedit, fugiat perferendis numquam nostrum debitis
+      temporibus aspernatur. Atque velit inventore repellendus consequatur, amet
+      facere neque vel, saepe asperiores sed possimus commodi quae ipsa
+      architecto voluptatem aperiam voluptates ullam tenetur. Vel in, nisi velit
+      temporibus quod cupiditate nesciunt quasi enim.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore a
+      quibusdam est debitis rem. Eius quas nihil provident cumque quos
+      reprehenderit animi illo sunt? Laborum repellendus quaerat quod possimus,
+      ipsum itaque deleniti molestias minima eaque illum dolores optio quo neque
+      perferendis id quam ab iusto dolorum reprehenderit sequi, natus quas error
+      non ut! Minus eum obcaecati maxime dolor maiores adipisci ex quasi, minima
+      illo, nam sequi cumque et? Inventore minima delectus esse quod nesciunt
+      unde quos, provident atque voluptates beatae dicta excepturi deleniti
+      impedit corporis illum! Totam officiis consectetur sunt doloribus dolorum
+      quae ab optio repudiandae, necessitatibus voluptatibus labore a?
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
